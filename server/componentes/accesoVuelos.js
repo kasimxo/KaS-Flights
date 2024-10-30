596eaf51fced09f40aa05298b9af9d45dfedf481
kasimxo <kasimxo@gmail.com>
2024-10-30 13:40:29 +0100

    0.5

--- server/componentes/accesoVuelos.js
+++ server/componentes/accesoVuelos.js
@@ -1,14 +1,18 @@
 import fs from 'node:fs'
+import path from 'path'
+
 
 // guardamos la información de los vuelos en un archivo.json
 export function guardarVuelos(vuelos) {
     let vuelosString = JSON.stringify(vuelos)
-    fs.writeFileSync('./vuelos.json', vuelosString, { encoding: 'utf8' })
+    //fs.writeFileSync('./vuelos.json', vuelosString, { encoding: 'utf8' })
 }
 
 // leemos la información de los vuelos del archivo.json
 export function leerVuelos() {
-    let vuelosRaw = fs.readFileSync('./vuelos.json', { encoding: 'utf8' })
+    let vuelosPath = path.join(process.cwd(), '..', 'vuelos.json');
+    console.log(vuelosPath)
+    let vuelosRaw = fs.readFileSync(vuelosPath, { encoding: 'utf8' })
     let vuelos = JSON.parse(vuelosRaw)
     console.log('Recuperados vuelos del archivo: ', vuelos.states.length)
     return vuelos
